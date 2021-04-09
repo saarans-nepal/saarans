e9d2c6ca8b7ad137be2f4ce9d0b3dac8970749dd
Himanshu Sah <itsmehs07@gmail.com>
2021-04-09 12:06:10 +0545

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const contactPopup = document.getElementById("contactPopup");
+contactPopup.style.display = "none";
+
+function showContact() {
+  contactPopup.style.display = "flex";
+}
+function hideContact() {
+  contactPopup.style.display = "none";
+}
